1b3369ba475ea21c6d2bd6e39ff0dd09eb972528
Ben Taylor <63203762+BenHiramTaylor@users.noreply.github.com>
2021-06-04 23:30:24 +0100

    refactored some paths, returning formatted json back

--- handlers/post.go
+++ handlers/post.go
@@ -1,19 +1,21 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/BenHiramTaylor/JSONToBigQuery/avro"
-	"github.com/BenHiramTaylor/JSONToBigQuery/dataTypes"
+	"github.com/BenHiramTaylor/JSONToBigQuery/data"
 	"github.com/go-playground/validator"
 )
 
 func JtBPost(w http.ResponseWriter, r *http.Request) {
-	jtaData := dataTypes.NewJTB()
+	jtaData := data.NewJTB()
 
 	if err := jtaData.LoadFromJSON(r); err != nil {
-		ErrorWithJSON(w, fmt.Sprintf("JSON data is invalid: %v", err.Error()), http.StatusBadRequest)
+		data.ErrorWithJSON(w, fmt.Sprintf("JSON data is invalid: %v", err.Error()), http.StatusBadRequest)
 		return
 	}
 	err := jtaData.Validate()
@@ -22,9 +24,18 @@ func JtBPost(w http.ResponseWriter, r *http.Request) {
 		for _, err := range err.(validator.ValidationErrors) {
 			errSlice = append(errSlice, fmt.Sprintf("Key: %v is invalid, got value: %v", err.Field(), err.Value()))
 		}
-		ErrorWithJSON(w, errSlice, http.StatusBadRequest)
+		data.ErrorWithJSON(w, errSlice, http.StatusBadRequest)
 		return
 	}
-	err := avro.ParseRequest(jtaData)
+	log.Printf("%#v", jtaData)
+	s, err := avro.ParseRequest(jtaData)
+	// THIS IS TEST LOGIC TO RETURN SAME ITEM
+	sJSON, err := json.Marshal(s)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Header().Add("Content-Type", "application/json; charset=utf-8")
+	w.Write(sJSON)
 	// TODO PARSE LOGIC HERE
 }
