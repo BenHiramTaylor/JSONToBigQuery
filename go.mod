bf5e52bcbb302a7f0f232e06a299ec80787c54b5
Ben Taylor <63203762+BenHiramTaylor@users.noreply.github.com>
2021-06-04 18:23:26 +0100

    updated some things

--- go.mod
+++ go.mod
@@ -1,8 +1,11 @@
-module JSONToBigQuery
+module github.com/BenHiramTaylor/JSONToBigQuery
 
 go 1.16
 
 require (
 	cloud.google.com/go/bigquery v1.18.0 // indirect
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/go-playground/validator v9.31.0+incompatible
 	github.com/gorilla/mux v1.8.0
+	github.com/leodido/go-urn v1.2.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,58 +1,18 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/BenHiramTaylor/JSONToBigQuery/handlers"
 	"github.com/gorilla/mux"
 )
-
-type jtaRequest struct {
-	ProjectID   string                   `json:"ProjectID"`
-	DatasetName string                   `json:"DatasetName"`
-	TableName   string                   `json:"TableName"`
-	Data        []map[string]interface{} `json:"Data"`
-}
-
-func helloWorld(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello World!")
-}
-
-func TestJSONPost(w http.ResponseWriter, r *http.Request) {
-	var jtaData jtaRequest
-
-	defer r.Body.Close()
-	err := json.NewDecoder(r.Body).Decode(&jtaData)
-	if err != nil {
-		http.Error(w, "JSON Data is invalid.", http.StatusBadRequest)
-		return
-	}
-	if len(jtaData.ProjectID) == 0 {
-		http.Error(w, "JSON Data is invalid, ProjectID is missing.", http.StatusBadRequest)
-		return
-	} else if len(jtaData.DatasetName) == 0 {
-		http.Error(w, "JSON Data is invalid, DatasetName is missing.", http.StatusBadRequest)
-		return
-	} else if len(jtaData.TableName) == 0 {
-		http.Error(w, "JSON Data is invalid, TableName is missing.", http.StatusBadRequest)
-		return
-	} else if len(jtaData.Data) == 0 {
-		http.Error(w, "JSON Data is invalid, Data array is empty.", http.StatusBadRequest)
-		return
-	}
-	err = json.NewEncoder(w).Encode(jtaData)
-	if err != nil {
-		panic(err)
-	}
-}
 
 func main() {
 	port := ":8080"
 	r := mux.NewRouter()
-	r.HandleFunc("/HelloWorld", helloWorld)
-	r.HandleFunc("/TestJSONPost", TestJSONPost).Methods("POST")
+	r.HandleFunc("/TestJSONPost", handlers.TestJSONPost).Methods("POST")
 	fmt.Println("Listening on port", port)
 	log.Fatal(http.ListenAndServe(port, r))
 }
